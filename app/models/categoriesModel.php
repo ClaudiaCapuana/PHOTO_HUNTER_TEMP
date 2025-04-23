d64fc1fb9b6164934aca24d9e23ae493eae73125
Claudia C <claudia.capuana@gmail.com>
2025-04-23 21:14:53 +0200

    categories._index +_search

---
+++ app/models/categoriesModel.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Models\CategoriesModel;
+use \PDO;
+
+function findAll(PDO $connexion) :array {
+$sql = "SELECT *
+        FROM categories 
+        ORDER BY created_at ASC;";
+$rs = $connexion->query($sql);
+return $rs->fetchAll(PDO::FETCH_ASSOC);
+}
