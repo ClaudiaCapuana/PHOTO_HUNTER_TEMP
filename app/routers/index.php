ef3df7eb4559a1eddc60e061cb7e4d13095f2ab3
Claudia C <claudia.capuana@gmail.com>
2025-04-23 22:55:38 +0200

    route authors.index avec partial

--- app/routers/index.php
+++ app/routers/index.php
@@ -1,11 +1,15 @@
 <?php
 
 
-
+//ROUTE PHOTOS
 if (isset ($_GET['photos'])) :
 include '../app/controllers/photosController.php';
 \App\Controllers\PhotosController\indexAction($connexion);
 
+//Route authors
+elseif (isset ($_GET['authors'])) :
+    include '../app/controllers/authorsController.php';
+\App\Controllers\AuthorsController\indexAction($connexion);
 
 else :
 include '../app/controllers/pagesController.php';
